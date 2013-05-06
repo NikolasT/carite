6c559e1a4ddedf64ef7ff7769811ddaff3f772f4
Nikolay Toloknov <nikolas.toloknov@gmail.com>
2013-05-06 17:09:11 +0300

    email config

--- Default.aspx.cs
+++ Default.aspx.cs
@@ -4,6 +4,8 @@ using System.Linq;
 using System.Web;
 using System.Web.UI;
 using System.Web.UI.WebControls;
+using System.Net;
+using System.Net.Mail;
 
 public partial class _Default : System.Web.UI.Page
 {
@@ -25,7 +27,72 @@ public partial class _Default : System.Web.UI.Page
 
         if (IsValid)
         {
-            HttpContext.Current.Session["order_id"] = String.Format("{0:d9}", (DateTime.Now.Ticks / 10000000)); 
+            HttpContext.Current.Session["order_id"] = String.Format("{0:d9}", (DateTime.Now.Ticks / 10000000));
+
+            var fromAddress = new MailAddress("carite@graphinity.com", "Carite");
+            //var toAddress = new MailAddress("support@graphinity.com", "Support");            
+            var toAddress = new MailAddress("nikolas.toloknov@gmail.com", "Support");
+            const string fromPassword = "fromPassword";
+            const string subject = "Subject";
+            string template =
+            @"
+            Order details, 
+            Ordered By: ##ORDERED_BY##
+            Email: ##DETAILS_EMAIL##
+            Quantity: ##QUANTITY##
+            Ship To: ##SHIP_TO##
+
+            Card front information,
+            Name: ##NAME##
+            Email: ##EMAIL##
+            Phone: ##PHONE##
+            Fax: ##FAX##
+            Address: ##ADDRESS##
+            City: ##CITY##
+            State: ##STATE##
+            Zip: ##ZIP##
+            OrderID: ##ORDER_ID##";             
+
+            string quantity = "";
+            bool isChecked = RadioButton1.Checked;
+            if(isChecked )
+              quantity=RadioButton1.Text;
+            else
+              quantity=RadioButton2.Text;
+
+            string body = template
+                .Replace("##ORDERED_BY##", ordered_by.Text)
+                .Replace("##DETAILS_EMAIL##", email.Text)
+                .Replace("##QUANTITY##", quantity)
+                .Replace("##SHIP_TO##", DropDownList1.SelectedItem.Text)
+                .Replace("##NAME##", name.Text)
+                .Replace("##PHONE##", phone_part_one.Text + "-" + phone_part_two.Text + "-" + phone_part_three.Text)
+                .Replace("##FAX##", fax_part_one.Text + "-" + fax_part_two.Text + "-" + fax_part_three.Text)
+                .Replace("##ADDRESS##", address.Text)
+                .Replace("##EMAIL##", card_email.Text)
+                .Replace("##CITY##", city.Text)
+                .Replace("##STATE##", DropDownListState.SelectedItem.Text)
+                .Replace("##ZIP##", zip.Text)
+                .Replace("##ORDER_ID##", HttpContext.Current.Session["order_id"].ToString());
+
+            var smtp = new SmtpClient
+            {
+                Host = "smtp.gmail.com",
+                Port = 587,
+                EnableSsl = false,
+                DeliveryMethod = SmtpDeliveryMethod.Network,
+                UseDefaultCredentials = false,
+                Credentials = new NetworkCredential(fromAddress.Address, fromPassword)
+            };
+            using (var message = new MailMessage(fromAddress, toAddress)
+            {
+                Subject = subject,
+                Body = body
+            })
+            {
+                smtp.Send(message);
+            }
+
             Response.Redirect("thank_you.aspx", true);
         }
     }
